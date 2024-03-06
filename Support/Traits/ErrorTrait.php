604632fcdec0b2a8002a2fee49c2ca655e0eee62
kbdxbt <1194174530@qq.com>
2024-03-06 15:06:20 +0800

    fix

--- Support/Traits/ErrorTrait.php
+++ Support/Traits/ErrorTrait.php
@@ -9,28 +9,21 @@ trait ErrorTrait
 {
     /**
      * 错误信息
-     *
-     * @var string
      */
     protected string $error = '';
 
     /**
      * 设置错误信息
-     *
-     * @param string $error
-     *
-     * @return bool
      */
     protected function setError(string $error): bool
     {
         $this->error = $error ?: '未知错误';
+
         return false;
     }
 
     /**
      * 获取错误信息
-     *
-     * @return string
      */
     public function getError(): string
     {
@@ -39,11 +32,9 @@ trait ErrorTrait
 
     /**
      * 是否存在错误信息
-     *
-     * @return bool
      */
     public function hasError(): bool
     {
-        return !empty($this->error);
+        return ! empty($this->error);
     }
 }
